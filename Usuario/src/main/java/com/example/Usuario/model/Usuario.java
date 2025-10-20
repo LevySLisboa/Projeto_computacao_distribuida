64a857858af9be075d9f6262cf8633eedc676dee
MuriloGirao <142538264+MuriloGirao@users.noreply.github.com>
2025-10-20 10:20:17 -0300

    TecnicoAdm Completo

--- Usuario/src/main/java/com/example/Usuario/model/Usuario.java
+++ Usuario/src/main/java/com/example/Usuario/model/Usuario.java
@@ -6,17 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
 
-@Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Usuario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name,email,senhahash,classe;
-    @ElementCollection
-    @CollectionTable(name = "usuario_funcao", joinColumns = @JoinColumn(name = "usuario_id"))
-    @Column(name = "funcao")
-    private List<String> funcoes;
+
 }
